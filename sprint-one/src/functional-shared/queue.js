34d087e4232c5ada093fa6d42274164224fe851a
MKS-Student <mks.student@hackreactor.com>
2017-03-22 16:36:32 -0700

    Return zero when we do practically nothing!
    Srsly, size method.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage : {},
+    head : 0,
+    tail : 0
+  };
+
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
+  someInstance.size = queueMethods.size;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
 
-
+  },
+  dequeue: function(){
+    return aThing;
+  },
+  size: function(){
+    return this.tail - this.head;
+  }
+};
