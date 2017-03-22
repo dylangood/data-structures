e0d3c1893b55354097cad0c47c58bc12799a11f8
MKS-Student <mks.student@hackreactor.com>
2017-03-22 16:15:38 -0700

    Report a size 0, correct size adding...
    And some other tests passed - YAY

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+     storage : {},
+     numberStored : 0
+  };
+  //extend(someInstance, Stack.stackMethods);
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+  return someInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value){
+    this.storage[this.size] = value;
+    this.numberStored++;
+  },
+  pop: function(){
+    //
+  },
+  size: function(){
+    return this.numberStored;
+  }
+};
