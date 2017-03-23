5ac4abf6064bf1e95895380df5d608460a9a20d3
MKS-Student <mks.student@hackreactor.com>
2017-03-23 16:35:36 -0700

    Stack and Queue pass all tests in the pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,20 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.head = 0;
+  this.tail = 0;
+  this.storage = {};
 };
 
-
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.tail++] = value;
+};
+Queue.prototype.dequeue = function() {
+  if(this.head === this.tail) {
+    return;
+  }
+  var itemToDequeue = this.storage[this.head];
+  delete this.storage[this.head++];
+  return itemToDequeue;
+};
+Queue.prototype.size = function() {
+  return this.tail - this.head;
+}
