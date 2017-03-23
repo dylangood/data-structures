f96bcec669ff5ffbff6b189a43c477b1a39a557c
MKS-Student <mks.student@hackreactor.com>
2017-03-23 15:45:48 -0700

    Stack Passes all tests
    Yay

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.top = 0;
+  stack.storage = {};
+  return stack;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.top] = value;
+    this.top++;
+  },
+  pop: function() {
+    if(this.top === 0) {
+      return;
+    }
+    this.top--;
+    var itemToPop = this.storage[this.top];
+    delete this.storage[this.top];
+    return itemToPop;
+  },
+  size: function() {
+    return this.top;
+  }
+};
