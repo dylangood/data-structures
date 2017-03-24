de3d62849e49302240814339a474e4fc6a54442f
MKS-Student <mks.student@hackreactor.com>
2017-03-24 11:42:30 -0700

    Complete all tests for Tree
    Not so granular, but a win is a win.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,28 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
   return newTree;
 };
 
-var treeMethods = {};
-
-treeMethods.addChild = function(value) {
+var treeMethods = {
+  addChild: function(value) {
+    var newTree = Tree(value);
+    this.children.push(newTree);
+  },
+  contains: function(target) {
+    var found = false;
+    for ( var i = 0; i < this.children.length; i++ ) {
+      found = found || this.children[i].contains(target);
+    }
+    return found || target === this.value;
+  }
 };
-
-treeMethods.contains = function(target) {
-};
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild: constant
+ contains: linear
  */
