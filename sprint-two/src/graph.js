8e7af93b552a00c3c2be339c6c41abd9e7810f40
MKS-Student <mks.student@hackreactor.com>
2017-03-24 21:05:30 -0700

    Make Graph work the best way we knew how
    Most of it's object; we felt like forEachNode could probably be used
    better, especially during removeNode, but it was getting hairy.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -7,11 +7,7 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  // Create a new node
-  // Give the node a node property
-  this.storage[node] = [];
-      // Give the node edges
-      // If no connection provided, use baseNode
+  this.storage[node] = {};
 };
 
 // Return a boolean node indicating if the node passed to contains is represented in the graph.
@@ -21,49 +17,43 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  for ( var i = 0; i < this.storage[node].length; i++ ) {
-    this.removeEdge( node, this.storage[node][i] );
+  for ( var key in this.storage[node] ) {
+    this.removeEdge( node, key );
   }
-  delete this.storage[node]
+  delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  for ( var i = 0; i < this.storage[fromNode].length; i++ ) {
-    if ( this.storage[fromNode][i] === toNode ) {
-      return true;
-    }
-  }
-  return false;
+  return this.storage[fromNode][toNode] || false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this.storage[fromNode].push(toNode);
-  this.storage[toNode].push(fromNode);
+  this.storage[fromNode][toNode] = true;
+  this.storage[toNode][fromNode] = true;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  for ( var i = 0; i < this.storage[fromNode].length; i++ ) {
-    if ( this.storage[fromNode][i] === toNode ) {
-      this.storage[fromNode][i] = undefined;
-    }
-  }
-  for ( var i = 0; i < this.storage[toNode].length; i++ ) {
-    if ( this.storage[toNode][i] === fromNode ) {
-      this.storage[toNode][i] = undefined;
-    }
-  }
+  delete this.storage[fromNode][toNode];
+  delete this.storage[toNode][fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
   for ( var key in this.storage ) {
-    cb(this.storage[key]);
+    cb( key );
   }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addNode: constant
+ contains: constant
+ removeNode: linear
+ hasEdge: constant
+ addEdge: constant
+ removeEdge: constant
+ forEachNode: linear
  */
