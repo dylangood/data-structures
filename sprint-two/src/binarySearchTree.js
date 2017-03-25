2a04653ec46dce99736591eff2bfae8294e4f994
MKS-Student <mks.student@hackreactor.com>
2017-03-25 15:43:02 -0700

    Add methods for generic BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,22 @@
 var BinarySearchTree = function(value) {
+  var bst = Object.create(BinarySearchTree.prototype);
+  bst.value = value;
+  bst.left = undefined;
+  bst.right = undefined;
+  return bst;
 };
 
+BinarySearchTree.prototype = {
+
+  insert: function(value) {
+
+  },
+  contains: function(value) {
+    return; //someBoolean
+  },
+  depthFirstLog: function(cb) {
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
